ec2da8b59015867168337a80b4faf4773dc0f008
salim680 <blkadisalim@gmail.com>
2026-06-08 15:01:35 +0100

    Update auth.php

--- backend/auth.php
+++ backend/auth.php
@@ -4,7 +4,7 @@
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: https://aladlyfamily.kesug.com'); // محدد وليس *
 header('Access-Control-Allow-Methods: POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(200); exit(); }
 
